a964e4a50c2198d483e73beb323f16e1fe8e3aff
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2021-02-15 18:14:19 -0600

    Initial commit

---
+++ aspnetcore-apexcharts/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aspnetcore_apexcharts
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            var color =  System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
+        }
+    }
+}
